d6d8fd552e22d6be566a0b6aa33b67c82628b74b
Alice <freshjs.ofte@gmail.com>
2022-02-21 18:58:52 +0200

    Install react and start

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import Component from './Component';
 //const Component = require('./Component');
 import './css/style.css';
 //require('./css/style.css');
-import sumInnerFunc from './math';
+//import {sum, pow, sub, div, multy} from './math';
+import twentyThree, * as MyMath from './math';
 
 const container = document.getElementById('root');
 //const res = sum(2,2);
+console.log(Component);
 
-console.log(sumInnerFunc(5));
+console.log(MyMath);
+console.log(twentyThree);
 
-container.append(` And hello again - ${res}`);
+container.append(` And hello again`);
--- src/math.js
+++ src/math.js
@@ -1,15 +1,15 @@
-const sum = (a,b) => a+b;
+ export const sum = (a,b) => a+b;
 
-const secret = 'secret value';
+export const multy = (a, b) => a*b;
 
-function sumWithValue() {
-    const value = 5;
-    return function sumValue(a) {
-        return a+value;
-    }
-}
+export const div = (a,b) => a/b;
 
-const sumInnerFunc = sumWithValue();
+export const sub = (a, b) => a-b;
 
+export const pow = (a,b) => a**b;
 
-export default sumInnerFunc
+const value = 23;
+
+export default value;
+
+//export default {sum, multy, div, sub, pow}
